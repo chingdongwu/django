bfe5de2684a052a6bbbf4b4f08fd475484350de8
dd <remix@gmail.com>
2022-10-05 21:30:41 +0800

    '完成密碼程式'

--- generator/views.py
+++ generator/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
+import random
+
+
 # Create your views here.
 def index(request):
     print('hello django')
@@ -9,7 +14,33 @@ def index(request):
 
 def password(request):
 
-    return render(request,'./password.html',{'password':'1234568'})
+    chars= [chr(c) for c in range(97,123)]
+    print(chars)
+    # input_length=request.GET.get('input-length')
+    # if input_length =='':
+    #     input_length=request.GET.get('length')
+        # print(input_length)
+    if request.GET.get('uppercase'):
+        chars+=[chr(c).upper() for c in range(97,123)]
+
+    print(chars)
+
+    if request.GET.get('numbers'):
+        chars+=[str(c) for c in range(0,11)]
+    print(chars)
+
+    if request.GET.get('special'):
+        chars+=list('@#!$%^&*')
+    print(chars)
+
+    length=eval(request.GET.get('length')) if request.GET.get('input-length')==''\
+        else eval(request.GET.get('input-length'))
+
+    password=''.join([random.choice(chars) for i in range(length)])
+    print(password)
+    print(length)
+
+    return render(request,'./password.html',{'password':password})
 
 
 
